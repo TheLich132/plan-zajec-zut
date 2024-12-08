7988b85bfde012c8da57797fa9e6f6a2b5045c34
Jakub Sumpor <j.sumpor@protonmail.com>
2024-12-08 18:38:20 +0100

    Left sidebar - switching schedules

--- assets/js/snippets.js
+++ assets/js/snippets.js
@@ -7,3 +7,45 @@ if (toastTrigger) {
         toastBootstrap.show()
     })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const scheduleName = document.getElementById('scheduleName');
+    const firstScheduleForm = document.getElementById('firstScheduleForm');
+    const secondScheduleForm = document.getElementById('secondScheduleForm');
+    const switchScheduleRight = document.getElementById('switchScheduleRight');
+    const switchScheduleLeft = document.getElementById('switchScheduleLeft');
+
+    let currentForm = 'first';
+
+    function switchForm(nextForm) {
+        if (currentForm === nextForm) return;
+
+        if (nextForm === 'first') {
+            firstScheduleForm.classList.add('active-form');
+            firstScheduleForm.classList.remove('inactive-form');
+            secondScheduleForm.classList.add('inactive-form');
+            secondScheduleForm.classList.remove('active-form');
+            currentForm = 'first';
+            switchScheduleLeft.disabled = true;
+            switchScheduleRight.disabled = false;
+            scheduleName.textContent = 'Plan 1';
+        } else if (nextForm === 'second') {
+            firstScheduleForm.classList.add('inactive-form');
+            firstScheduleForm.classList.remove('active-form');
+            secondScheduleForm.classList.add('active-form');
+            secondScheduleForm.classList.remove('inactive-form');
+            currentForm = 'second';
+            switchScheduleLeft.disabled = false;
+            switchScheduleRight.disabled = true;
+            scheduleName.textContent = 'Plan 2';
+        }
+    }
+
+    switchScheduleRight.addEventListener('click', () => {
+        switchForm('second');
+    });
+
+    switchScheduleLeft.addEventListener('click', () => {
+        switchForm('first');
+    });
+});
